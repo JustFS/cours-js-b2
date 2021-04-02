c84690acbf0c34de03017fa0dc1691ce1516d76d
Julien Az <julien.azbrg@gmail.com>
2021-04-02 15:31:51 +0200

    finished events

--- events/index.js
+++ events/index.js
@@ -19,6 +19,12 @@ mouseEvent.addEventListener("mousemove", (event) => {
   y.innerHTML = event.y;
   mouseEvent.style.left = event.x + "px";
   mouseEvent.style.top = event.y + "px";
+
+  if (event.x < 200) {
+    document.body.style.filter = "blur(2px)";
+  } else {
+    document.body.style.filter = "none";
+  }
 });
 
 // INPUT EVENT
@@ -31,9 +37,17 @@ themeButtons.forEach((button) => {
   button.addEventListener("click", (event) => {
     console.log(event.target.id);
 
+    document.body.classList.remove("dark-theme", "salmon-theme", "teal-theme");
+
     switch (event.target.id) {
       case "dark":
         document.body.classList.add("dark-theme");
+        break;
+      case "salmon":
+        document.body.classList.add("salmon-theme");
+        break;
+      case "teal":
+        document.body.classList.add("teal-theme");
         break;
 
       // toujours finir le switch par un default case
